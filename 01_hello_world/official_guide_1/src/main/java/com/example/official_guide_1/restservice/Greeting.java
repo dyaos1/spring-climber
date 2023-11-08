db59b18bd7c20c8ac1eca9405b8fcee55ee6e599
DESKTOP-AN010CN\Flzomb <spark3840@gmail.com>
2023-11-08 11:16:48 +0900

    feat(#3): rest service

---
+++ 01_hello_world/official_guide_1/src/main/java/com/example/official_guide_1/restservice/Greeting.java
@@ -0,0 +1,4 @@
+package com.example.official_guide_1.restservice;
+
+public record Greeting(long id, String content) {
+}
---
+++ 01_hello_world/official_guide_1/src/test/java/com/example/official_guide_1/OfficialGuide1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.official_guide_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialGuide1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
